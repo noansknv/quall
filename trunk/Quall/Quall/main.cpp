527fc9ae7b20a7289c95c1cd160a3b3fa5de874a
lesefer <lesefer@8a10c9d2-0e52-11df-8d37-15e3504968c6>
2010-01-31 16:55:50 +0000

    Bazowy projekt.

---
+++ trunk/Quall/Quall/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Glowny plik rozruchowy.
+ * @author Marcin Kanclerz
+ */
+#define WIN32_LEAN_AND_MEAN
+
+#include "windows.h"
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return 0;
+}
